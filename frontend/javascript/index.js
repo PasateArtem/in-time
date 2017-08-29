fa8891b06f23880ff1e1684deccd76fbc57dc9f6
Артем Пасатецкий < >
2017-08-29 21:12:59 +0300

    second commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+requirejs.config({
+    baseUrl: './js',
+    paths: {
+        
+    }
+});
+(function() {
+
+  "use strict";
+
+  var toggles = document.querySelectorAll(".cmn-toggle-switch");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+    });
+  }
+
+})();
+<!--[if lt IE 9]>
+
+    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+
+  <![endif]-->
+
+requirejs([
+	'jquery'
+], function($) {
+    
+});
